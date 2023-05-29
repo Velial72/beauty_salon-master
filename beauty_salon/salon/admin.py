4ba40194d7bdcab5a2da3003aec606f5231ee90e
Velial72 <126498671+Velial72@users.noreply.github.com>
2023-05-29 23:57:19 +0300

    Update admin.py

--- beauty_salon/salon/admin.py
+++ beauty_salon/salon/admin.py
@@ -4,9 +4,10 @@ from .forms import ClientsForm, SalonForm, MasterForm
 from .models import Clients, Salon, Master
 
 
+
 @admin.register(Clients)
 class ClientsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'master', 'service', 'price', 'visit_time')
+    list_display = ('id', 'td_id', 'name', 'client_phone_number')
     form = ClientsForm
 
 
